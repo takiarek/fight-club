c1b7bd232037e34cb9b7f3e3435d6e5983e5a189
takiarek <takiarek@gmail.com>
2016-03-01 20:36:12 +0100

    Make Fighters controller and basic views

---
+++ app/controllers/fighters_controller.rb
@@ -0,0 +1,28 @@
+class FightersController < ApplicationController
+  def index
+    @fighters = Fighter.all
+  end
+
+  def show
+    @fighter = Fighter.find(params[:id])
+  end
+
+  def new
+    @fighter = Fighter.new
+  end
+
+  def create
+    @fighter = Fighter.new(fighter_params)
+
+    if @fighter.save
+      redirect_to @fighter
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def fighter_params
+      params.require(:fighter).permit(:fname, :lname, :description)
+    end
+end
